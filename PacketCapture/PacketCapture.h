baa828b401283c386956a2072958c65e00a18934
lwonho92 <lwonho92@gmail.com>
2017-06-03 23:00:13 +0900

    Cehck

--- PacketCapture/PacketCapture.h
+++ PacketCapture/PacketCapture.h
@@ -1,32 +1,3 @@
-
-// PacketCapture.h : PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
-//
-
-#pragma once
-
-#ifndef __AFXWIN_H__
-#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
-#endif
-
-#include "resource.h"		// 주 기호입니다.
-
-
-// CPacketCaptureApp:
-// 이 클래스의 구현에 대해서는 PacketCapture.cpp을 참조하십시오.
-//
-
-class CPacketCaptureApp : public CWinApp
-{
-public:
-	CPacketCaptureApp();
-
-	// 재정의입니다.
-public:
-	virtual BOOL InitInstance();
-
-	// 구현입니다.
-
-	DECLARE_MESSAGE_MAP()
-};
-
-extern CPacketCaptureApp theApp;
+version https://git-lfs.github.com/spec/v1
+oid sha256:caccee003236426e647049edf05f190c7cb25f5a504aef43def5e2ac0785a597
+size 564
